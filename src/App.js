76eaa7f7a4bb1684b2e6208aeb16379b0c972ca8
Jorge Nunes <jlbnunes@live.cocom>
2021-07-24 18:41:07 -0300

    Adicionando corpo de mensagem

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import ChartListItem from './components/ChatListItem';
+import ChatIntro from './components/ChatIntro'
+import ChatWindow from './components/ChatWindow'
 
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
@@ -9,7 +11,13 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
 
 export default () => {
-	const [chatlist, setChatList] = useState([{},{},{},{},{},{},{},{},{},{},{},{}]);
+	const [chatlist, setChatList] = useState([
+		{chatId: 1, title: 'fulano de tal ', image:'avatar.png'},
+		{chatId: 2, title: 'Cigrano de tal ', image:'avatar.png'},
+		{chatId: 3, title: 'Tico de tal ', image:'avatar.png'},
+		{chatId: 4, title: 'fulano de tal ', image:'avatar.png'},
+	]);
+	const [activeChat, setActiveChat] = useState({});
 	return (
 	<div className="appWindow">
 		<div className="sidebar">
@@ -39,13 +47,21 @@ export default () => {
 				{chatlist.map((item, key) => (
 					<ChartListItem 
 						key={key}
+						data={item}
+						active={activeChat.chatId === chatlist[key].chatId}
+						onClick={()=>setActiveChat(chatlist[key])}
 					/>
 				))}
 			</div>
 
 		</div>
 		<div className="contentarea">
-			Ola Mundo ! -contentarea
+			{activeChat.chatId !== undefined && 
+				<ChatWindow />
+			}
+			{activeChat.chatId === undefined && 
+				<ChatIntro/>
+			}
 		</div>
 	</div>
 	);
